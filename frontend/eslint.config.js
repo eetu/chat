d6d0464f35a36c12fd160de7318890e23599b12f
Eetu Sutinen <eetu.sutinen@gmail.com>
2026-05-05 07:47:27 +0300

    Add router lint plugin

--- frontend/eslint.config.js
+++ frontend/eslint.config.js
@@ -1,3 +1,4 @@
+import pluginRouter from "@tanstack/eslint-plugin-router";
 import reactConfig from "eslint-config/react";
 
-export default reactConfig;
+export default [...pluginRouter.configs["flat/recommended"], ...reactConfig];
